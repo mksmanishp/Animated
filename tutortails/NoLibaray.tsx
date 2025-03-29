73fecc9298f53bb78b74c83e27202dc5db922274
manish.kumar <kumar.manish@medikabazaar.com>
2025-03-29 12:25:55 +0530

    first commit

---
+++ tutortails/NoLibaray.tsx
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+
+const NoLibaray = () => {
+    const [post, setPost] = useState(0)
+    useEffect(() => {
+        let interval: NodeJS.Timeout;
+        interval = setInterval(() => {
+            setPost(prevPost => (prevPost < 200) ? prevPost + 5 : 0)
+        }, 1000)
+        return () => clearInterval(interval);
+    }, [])
+    return (
+        <View style={styles.box}>
+        </View>
+    )
+}
+
+export default NoLibaray
+
+const styles = StyleSheet.create({
+    box: {
+        height: 50,
+        width: 50,
+        backgroundColor: 'yellow'
+    }
+})
